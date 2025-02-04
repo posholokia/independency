236311e017fc91a11d021c58d8c0c84d11b4c5b6
posholokia <posholokia@gmail.com>
2025-02-04 19:47:12 +0600

    feat: add new scope - cached, fix type hints

--- independency/container.py
+++ independency/container.py
@@ -19,7 +19,14 @@ from typing import (
 )
 
 _T = TypeVar('_T')
+CLS = TypeVar("CLS", Any, str)
 ObjType = Union[str, Type[_T]]
+
+
+class Scope(Enum):
+    transient: int = 0
+    singleton: int = 1
+    cached: int = 2
 
 
 class ContainerError(Exception):
@@ -30,7 +37,7 @@ class ContainerError(Exception):
 class Registration:
     cls: ObjType[Any]
     factory: Callable[..., Any]
-    is_singleton: bool
+    scope: Scope
     kwargs: Dict[str, Any]
 
 
@@ -126,65 +133,118 @@ def _update_localns(cls: ObjType[Any], localns: Dict[str, Any]) -> None:
         localns[cls] = cls
 
 
-class Container:  # pylint: disable=R0903
-    __slots__ = ["_registry", "_localns", "_resolved"]
+class ResolutionCache:
+    def __init__(self):
+        self.cache: dict[str, Any] = {}
 
-    def __init__(self, registry: Dict[ObjType[Any], Registration], localns: Dict[str, Any]):
+    def __setitem__(self, key, instance):
+        self.cache[key] = instance
+
+    def __getitem__(self, key) -> Any:
+        return self.cache.get(key)
+
+    def has_cached(self, key) -> bool:
+        return key in self.cache
+
+    def clear(self):
+        self.cache.clear()
+
+
+class Container:  # pylint: disable=R0903
+    __slots__ = ["_registry", "_localns", "_resolved", "_cache"]
+
+    def __init__(
+        self,
+        registry: Dict[ObjType[Any], Registration],
+        localns: Dict[str, Any],
+    ):
         self._registry = registry
         self._localns = localns
         self._resolved: Dict[ObjType[Any], Any] = {}
+        self._cache = ResolutionCache()
 
     def get_registered_deps(self) -> Set[ObjType[Any]]:
         return set(self._registry.keys())
 
-    def resolve(self, cls: ObjType[Any]) -> Any:
+    def resolve(self, cls: ObjType[type[CLS]]) -> CLS:
+        result = self.resolve_impl(cls)
+        self._cache.clear()
+        return result
+
+    def resolve_impl(self, cls: ObjType[type[CLS]]) -> CLS:
         cls = get_from_localns(cls, self._localns)
+
         if cls in self._resolved:
             return self._resolved[cls]
+
+        if self._cache.has_cached(cls):
+            return self._cache[cls]
 
         try:
             current = self._registry[cls]
         except KeyError as e:
-            raise ContainerError(f'No dependency of type {cls}') from e
+            raise ContainerError(f"No dependency of type {cls}") from e
 
         args = _resolve_constants(current.kwargs)
         deps_to_resolve = get_deps(current, self._localns)
         for key, d in deps_to_resolve.items():
-            args[key] = self.resolve(d)
+            args[key] = self.resolve_impl(d)
         result = current.factory(**args)
-        if current.is_singleton:
+        if current.scope is Scope.singleton:
             self._resolved[current.cls] = result
+        if current.scope is Scope.cached:
+            self._cache[cls] = result
         return result  # noqa: R504
 
-    def create_test_container(self) -> 'TestContainer':
+    def create_test_container(self) -> "TestContainer":
         registry = copy.deepcopy(self._registry)
         localns = copy.deepcopy(self._localns)
         test_container = TestContainer(registry=registry, localns=localns)
-        registry[Container] = Registration(Container, factory=lambda: test_container, is_singleton=True, kwargs={})
+        registry[Container] = Registration(
+            Container,
+            factory=lambda: test_container,
+            scope=Scope.singleton,
+            kwargs={},
+        )
         _update_localns(Container, localns)
         return test_container
 
 
 class TestContainer(Container):
     def with_overridden(
-        self, cls: ObjType[Any], factory: Callable[..., Any], is_singleton: bool, **kwargs: Any
-    ) -> 'TestContainer':
+        self,
+        cls: ObjType[Any],
+        factory: Callable[..., Any],
+        scope: Scope = Scope.transient,
+        **kwargs: Any,
+    ) -> "TestContainer":
         if cls not in self._registry:
-            raise ContainerError("Can not override class without any registration")
+            raise ContainerError(
+                "Can not override class without any registration"
+            )
         _validate_registration(cls, factory, kwargs)
         registry = copy.deepcopy(self._registry)
         localns = copy.deepcopy(self._localns)
         _update_localns(cls, localns)
-        registry[cls] = Registration(cls=cls, factory=factory, kwargs=kwargs, is_singleton=is_singleton)
+        registry[cls] = Registration(
+            cls=cls, factory=factory, kwargs=kwargs, scope=scope
+        )
         container = TestContainer(registry, localns)
-        registry[Container] = Registration(Container, factory=lambda: container, is_singleton=True, kwargs={})
+        registry[Container] = Registration(
+            Container,
+            factory=lambda: container,
+            scope=Scope.singleton,
+            kwargs={},
+        )
         _update_localns(Container, localns)
         return container
 
     def with_overridden_singleton(
         self, cls: ObjType[Any], factory: Callable[..., Any], **kwargs: Any
-    ) -> 'TestContainer':
-        return self.with_overridden(cls, factory, is_singleton=True, **kwargs)
+    ) -> "TestContainer":
+        return self.with_overridden(
+            cls, factory, scope=Scope.singleton, **kwargs
+        )
 
 
 class ContainerBuilder:
@@ -198,19 +258,36 @@ class ContainerBuilder:
         registry = self._registry.copy()
         localns = self._localns.copy()
         container = Container(registry=registry, localns=localns)
-        registry[Container] = Registration(cls=Container, factory=lambda: container, kwargs={}, is_singleton=True)
+        registry[Container] = Registration(
+            cls=Container,
+            factory=lambda: container,
+            kwargs={},
+            scope=Scope.singleton,
+        )
         _update_localns(Container, localns)
         self._check_resolvable(registry, localns)
         return container
 
-    def singleton(self, cls: ObjType[Any], factory: Callable[..., Any], **kwargs: Any) -> None:
-        self.register(cls=cls, factory=factory, is_singleton=True, **kwargs)
+    def singleton(
+            self, cls: ObjType[Any], factory: Callable[..., Any], **kwargs: Any
+    ) -> None:
+        self.register(
+            cls=cls, factory=factory, scope=Scope.singleton, **kwargs
+        )
 
-    def register(self, cls: ObjType[Any], factory: Callable[..., Any], is_singleton: bool, **kwargs: Any) -> None:
+    def register(
+            self,
+            cls: ObjType[Any],
+            factory: Callable[..., Any],
+            scope: Scope = Scope.transient,
+            **kwargs: Any,
+    ) -> None:
         if cls in self._registry:
-            raise ContainerError(f'Type {cls} is already registered')
+            raise ContainerError(f"Type {cls} is already registered")
         _validate_registration(cls, factory, kwargs)
-        self._registry[cls] = Registration(cls=cls, factory=factory, kwargs=kwargs, is_singleton=is_singleton)
+        self._registry[cls] = Registration(
+            cls=cls, factory=factory, kwargs=kwargs, scope=scope
+        )
         _update_localns(cls, self._localns)
 
     def _check_resolvable(self, registry: Dict[ObjType[Any], Registration], localns: Dict[str, Any]) -> None:
